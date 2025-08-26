f4f1189c7172d2af18ca011377df6a78960ba23c
shekhmohibur <mohibur.rahman2003@gmail.com>
2025-08-26 20:14:29 +0600

    footer section perfectly added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,32 @@
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="bg-white pt-3 mt-16">
+            <div className="flex flex-col items-center">
+                <h1 className="md:text-3xl font-semibold text-black">Gadget Heaven</h1>
+                <p className="text-gray-500 text-center">Leading the way in cutting-edge technology and innovation.</p>
+            </div>
+            <div className="footer sm:footer-horizontal bg-white text-neutral-content p-10">
+                <nav className="text-gray-500">
+                    <h6 className="font-semibold text-black capitalize">Services</h6>
+                    <a className="link link-hover">Product Support</a>
+                    <a className="link link-hover">Order Tracking</a>
+                    <a className="link link-hover">Shipping & Delivery</a>
+                    <a className="link link-hover">Returns</a>
+                </nav>
+                <nav className="text-gray-500">
+                    <h6 className="font-semibold text-black capitalize">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Careers</a>
+                    <a className="link link-hover">Contact</a>
+                </nav>
+                <nav className="text-gray-500">
+                    <h6 className="font-semibold text-black capitalize">Legal</h6>
+                    <a className="link link-hover">Terms of Services</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </div>
+        </footer>
     );
 };
 
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,7 +1,13 @@
 const Statistics = () => {
     return (
         <div>
-            <h1>its statistics</h1>
+            <div className="bg-[#9538E2] py-10 flex flex-col items-center gap-5">
+                <h1 className="text-white text-2xl md:text-4xl font-semibold">Statistics</h1>
+                <p className="max-w-[796px] text-center text-gray-300">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+            </div>
+            <div className={`flex justify-between px-6 items-center py-8`}>
+                <h2 className="font-semibold">Statistics</h2>
+            </div>
         </div>
     );
 };
